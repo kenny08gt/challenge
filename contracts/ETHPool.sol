44e1ca13e9709ba68a2ff2b44c465feb4c682156
Alan Hurtarte <alan@mypostcard.com>
2022-03-17 23:29:27 -0600

    Improve the split logic. Remove the loop

--- contracts/ETHPool.sol
+++ contracts/ETHPool.sol
@@ -9,63 +9,73 @@ contract ETHPool is AccessControl {
     using PRBMathUD60x18 for uint256;
     using PRBMathSD59x18 for int256;
 
+    struct userDeposit {
+        uint256 deposit;
+        uint256 dateDeposit;
+    }
+
     bytes32 public constant ROLE_TEAM = keccak256("ROLE_TEAM");
     bytes32 public constant ROLE_USER = keccak256("ROLE_USER");
     uint256 public reward_pool = 0;
     uint256 public total_pool = 0;
-    uint256 public total_users = 0;
-    mapping(address => uint256) users_pool;
-    mapping(uint256 => address) private holders;
-    mapping(address => uint256) private shares;
+    uint256 public lastRewardDate;
+
+    mapping(address => userDeposit) users_pool;
 
     constructor() {
+        lastRewardDate = block.timestamp;
         _grantRole(ROLE_TEAM, msg.sender);
     }
 
     function deposit() public payable {
         require(hasRole(ROLE_USER, msg.sender), "Only user can deposit");
         require(msg.value > 0, "Amount should be higher than 0");
-        if (users_pool[msg.sender] == 0) {
-            holders[total_users] = msg.sender;
-            users_pool[holders[total_users]] = msg.value;
-            total_users = total_users + 1;
-        } else {
-            users_pool[msg.sender] = users_pool[msg.sender] + msg.value;
-        }
+
+        users_pool[msg.sender].deposit = msg.value;
+        users_pool[msg.sender].dateDeposit = block.timestamp;
+
         total_pool = total_pool + msg.value;
     }
 
     function withdraw() public {
         require(hasRole(ROLE_USER, msg.sender), "Only user can withdrwa");
-        require(users_pool[msg.sender] > 0, "The user dont have balance");
+        require(
+            users_pool[msg.sender].deposit > 0,
+            "The user dont have balance"
+        );
 
-        total_users = total_users - 1;
-        total_pool = total_pool - users_pool[msg.sender];
-        payable(msg.sender).transfer(users_pool[msg.sender]);
-        users_pool[msg.sender] = 0;
+        if (users_pool[msg.sender].dateDeposit < lastRewardDate) {
+            uint256 user_pool_deposit = users_pool[msg.sender].deposit;
+            uint256 share = user_pool_deposit.div(total_pool);
+            uint256 total_withdraw = user_pool_deposit + reward_pool.mul(share);
+            payable(msg.sender).transfer(total_withdraw);
+            total_pool = total_pool - user_pool_deposit;
+            reward_pool = reward_pool - reward_pool.mul(share);
+        } else {
+            payable(msg.sender).transfer(users_pool[msg.sender].deposit);
+        }
+
+        users_pool[msg.sender].deposit = 0;
     }
 
     function balance() public view returns (uint256) {
-        return users_pool[msg.sender];
+        require(hasRole(ROLE_USER, msg.sender), "Only user can have balance");
+        if (users_pool[msg.sender].dateDeposit < lastRewardDate) {
+            uint256 user_pool_deposit = users_pool[msg.sender].deposit;
+            uint256 share = user_pool_deposit.div(total_pool);
+            uint256 total_withdraw = user_pool_deposit + reward_pool.mul(share);
+            return total_withdraw;
+        } else {
+            return users_pool[msg.sender].deposit;
+        }
     }
 
     function depositReward() public payable {
         require(hasRole(ROLE_TEAM, msg.sender), "Only team can deposit reward");
         require(msg.value > 0, "Amount should be higher than 0");
-        require(total_users > 0, "Cannot split reward without users");
 
         reward_pool = reward_pool + msg.value;
-        uint256 new_total = 0;
-
-        for (uint256 i = 0; i < total_users; i++) {
-            uint256 user_pool = users_pool[holders[i]];
-            uint256 share = user_pool.div(total_pool);
-            users_pool[holders[i]] = user_pool + reward_pool.mul(share);
-            new_total = new_total + user_pool + reward_pool.mul(share);
-        }
-
-        total_pool = new_total;
-        reward_pool = 0;
+        lastRewardDate = block.timestamp;
     }
 
     function balanceReward() public view returns (uint256) {
--- test/ethPool.js
+++ test/ethPool.js
@@ -28,15 +28,12 @@ contract('ETHPool', (accounts) => {
 
     it('Team deposit reward but split fail because no users exists', async () => {
         const ETHPoolInstance = await ETHPool.deployed();
-        try {
-            await ETHPoolInstance.depositReward({ from: owner, value: web3.utils.toWei(String(1), 'ether') });
-            assert.equal(true, false);
-        } catch (error) {
-            assert.equal(true, true);
-        }
+        await ETHPoolInstance.depositReward({ from: owner, value: web3.utils.toWei(String(1), 'ether') });
+        assert.equal(true, true);
+
         const balance = await ETHPoolInstance.balanceReward.call({ from: owner });
         // console.log(BigInt(balance));
-        assert.equal(BigInt(balance), BigInt(web3.utils.toWei(String(0), 'ether')));
+        assert.equal(BigInt(balance), BigInt(web3.utils.toWei(String(1), 'ether')));
         // await ETHPoolInstance.withdraw({ from: user });
     });
 
@@ -57,6 +54,7 @@ contract('ETHPool', (accounts) => {
         await ETHPoolInstance.addUserRole(user, { from: owner });
         await ETHPoolInstance.deposit({ from: user, value: web3.utils.toWei(String(0.2), 'ether') });
         let balance = await ETHPoolInstance.balance.call({ from: user });
+        // console.log(BigInt(balance));
         assert.equal(BigInt(balance), BigInt(web3.utils.toWei(String(0.2), 'ether')));
 
 
@@ -64,24 +62,30 @@ contract('ETHPool', (accounts) => {
         await ETHPoolInstance.addUserRole(user2, { from: owner });
         await ETHPoolInstance.deposit({ from: user2, value: web3.utils.toWei(String(0.30), 'ether') });
         let balance2 = await ETHPoolInstance.balance.call({ from: user2 });
+        // console.log(BigInt(balance2));
         assert.equal(BigInt(balance2), BigInt(web3.utils.toWei(String(0.30), 'ether')));
 
 
-        await ETHPoolInstance.depositReward({ from: owner, value: web3.utils.toWei(String(1), 'ether') });
-        let balance3 = await ETHPoolInstance.balanceReward.call({ from: owner });
-        assert.equal(BigInt(balance3), BigInt(web3.utils.toWei(String(0), 'ether')));
+        setTimeout(async () => {
+            await ETHPoolInstance.depositReward({ from: owner, value: web3.utils.toWei(String(1), 'ether') });
+            let balance3 = await ETHPoolInstance.balanceReward.call({ from: owner });
+            // console.log(BigInt(balance3));
+            assert.equal(BigInt(balance3), BigInt(web3.utils.toWei(String(1), 'ether')));
 
-        balance = 0;
-        balance = await ETHPoolInstance.balance.call({ from: user });
-        // console.log(BigInt(balance));
-        assert.equal(BigInt(balance), BigInt(web3.utils.toWei(String(0.6), 'ether')), "User 1 should have 0.7 ether");
-        await ETHPoolInstance.withdraw({ from: user });
+            balance = 0;
+            balance = await ETHPoolInstance.balance.call({ from: user });
+            // console.log(BigInt(balance));
+            assert.equal(BigInt(balance), BigInt(web3.utils.toWei(String(0.6), 'ether')), "User 1 should have 0.7 ether");
+            await ETHPoolInstance.withdraw({ from: user });
 
-        balance2 = 0;
-        balance2 = await ETHPoolInstance.balance.call({ from: user2 });
-        // console.log(BigInt(balance2));
-        assert.equal(BigInt(balance2), BigInt(web3.utils.toWei(String(0.9), 'ether')), "User 2, shoudl have 0.9 ether");
-        await ETHPoolInstance.withdraw({ from: user2 });
+            balance2 = 0;
+            balance2 = await ETHPoolInstance.balance.call({ from: user2 });
+            // console.log(BigInt(balance2));
+            assert.equal(BigInt(balance2), BigInt(web3.utils.toWei(String(0.9), 'ether')), "User 2, shoudl have 0.9 ether");
+            await ETHPoolInstance.withdraw({ from: user2 });
+        }, 500)
+
+
     });
 
     it('User A deposit, team deposit reward, User B deposit', async () => {
@@ -93,28 +97,30 @@ contract('ETHPool', (accounts) => {
         assert.equal(BigInt(balance), BigInt(web3.utils.toWei(String(0.2), 'ether')));
 
 
-        await ETHPoolInstance.depositReward({ from: owner, value: web3.utils.toWei(String(1), 'ether') });
-        let balance3 = await ETHPoolInstance.balanceReward.call({ from: owner });
-        assert.equal(BigInt(balance3), BigInt(web3.utils.toWei(String(0), 'ether')));
+        setTimeout(async () => {
+            await ETHPoolInstance.depositReward({ from: owner, value: web3.utils.toWei(String(1), 'ether') });
+            let balance3 = await ETHPoolInstance.balanceReward.call({ from: owner });
+            assert.equal(BigInt(balance3), BigInt(web3.utils.toWei(String(0), 'ether')));
 
-        const user2 = accounts[2];
-        await ETHPoolInstance.addUserRole(user2, { from: owner });
-        await ETHPoolInstance.deposit({ from: user2, value: web3.utils.toWei(String(0.30), 'ether') });
-        let balance2 = await ETHPoolInstance.balance.call({ from: user2 });
-        assert.equal(BigInt(balance2), BigInt(web3.utils.toWei(String(0.30), 'ether')));
+            const user2 = accounts[2];
+            await ETHPoolInstance.addUserRole(user2, { from: owner });
+            await ETHPoolInstance.deposit({ from: user2, value: web3.utils.toWei(String(0.30), 'ether') });
+            let balance2 = await ETHPoolInstance.balance.call({ from: user2 });
+            assert.equal(BigInt(balance2), BigInt(web3.utils.toWei(String(0.30), 'ether')));
 
 
+            balance = 0;
+            balance = await ETHPoolInstance.balance.call({ from: user });
+            // console.log(BigInt(balance));
+            assert.equal(BigInt(balance), BigInt(web3.utils.toWei(String(1.2), 'ether')), "User 1 should have 0.7 ether");
+            await ETHPoolInstance.withdraw({ from: user });
 
-        balance = 0;
-        balance = await ETHPoolInstance.balance.call({ from: user });
-        // console.log(BigInt(balance));
-        assert.equal(BigInt(balance), BigInt(web3.utils.toWei(String(1.2), 'ether')), "User 1 should have 0.7 ether");
-        await ETHPoolInstance.withdraw({ from: user });
+            balance2 = 0;
+            balance2 = await ETHPoolInstance.balance.call({ from: user2 });
+            // console.log(BigInt(balance2));
+            assert.equal(BigInt(balance2), BigInt(web3.utils.toWei(String(0.3), 'ether')), "User 2, shoudl have 0.9 ether");
+            await ETHPoolInstance.withdraw({ from: user2 });
+        }, 500)
 
-        balance2 = 0;
-        balance2 = await ETHPoolInstance.balance.call({ from: user2 });
-        // console.log(BigInt(balance2));
-        assert.equal(BigInt(balance2), BigInt(web3.utils.toWei(String(0.3), 'ether')), "User 2, shoudl have 0.9 ether");
-        await ETHPoolInstance.withdraw({ from: user2 });
     });
 });
